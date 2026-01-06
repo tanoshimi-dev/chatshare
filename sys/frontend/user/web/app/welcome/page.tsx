ed23322d0e1a84fe06f1f1fde65b4f6195ea6f8b
tanoshimi-dev <keijimitaki@gmail.com>
2026-01-07 07:55:39 +0900

    [app-assets]welcomeページの修正 アプリイメージの追加

--- sys/frontend/user/web/app/welcome/page.tsx
+++ sys/frontend/user/web/app/welcome/page.tsx
@@ -105,6 +105,48 @@ export default function Home() {
                         </div>
                     </section>
                 </div>
+
+                <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+                    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                        Share your chats
+                    </h1>
+                    <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+                        Looking for a starting point or more instructions? Head
+                        over to{" "}
+                        <a
+                            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                            className="font-medium text-zinc-950 dark:text-zinc-50"
+                        >
+                            Templates
+                        </a>{" "}
+                        or the{" "}
+                        <a
+                            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                            className="font-medium text-zinc-950 dark:text-zinc-50"
+                        >
+                            Learning
+                        </a>{" "}
+                        center.
+                    </p>
+                </div>
+
+                <div className="mt-4 flex flex-row min-w-full items-center justify-center">
+                    <Image
+                        src="/images/emulator-image-1.png"
+                        alt="app image 1"
+                        width={280}
+                        height={280}
+                        priority
+                    />
+
+                    <Image
+                        src="/images/emulator-image-2.png"
+                        alt="app image 2"
+                        width={280}
+                        height={280}
+                        priority
+                    />
+                </div>
             </main>
         </div>
     );
