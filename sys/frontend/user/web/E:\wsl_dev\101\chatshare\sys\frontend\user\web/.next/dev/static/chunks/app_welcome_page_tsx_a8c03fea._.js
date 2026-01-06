dd75abae8893f0780b4305dccecfe92a5f433f69
tanoshimi-dev <keijimitaki@gmail.com>
2026-01-06 22:51:15 +0900

    [app-assets]不要なディレクトリを削除

--- sys/frontend/user/web/E:\wsl_dev\101\chatshare\sys\frontend\user\web/.next/dev/static/chunks/app_welcome_page_tsx_a8c03fea._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_7b51109e._.js",
-  "static/chunks/app_welcome_TypingChatOverlay_tsx_6cdcc7e6._.js"
-],
-    source: "dynamic"
-});
